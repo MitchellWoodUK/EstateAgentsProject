0197b37c19e2b4b938d9560e3375b1c93a945bf6
Mitchell Wood <WOODM@bridgwater.ac.uk>
2024-12-10 16:23:46 +0000

    Added custom inputs to identity and begun coding user roles

--- EstateAgents/Data/ApplicationDbContext.cs
+++ EstateAgents/Data/ApplicationDbContext.cs
@@ -1,13 +1,52 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EstateAgents.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EstateAgents.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<CustomUserModel>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+
+        //Seed Roles Below
+        /*private override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }*/
+
+        private void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Admin",
+                    NormalizedName = "Admin".ToUpper()
+                }
+                );
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Staff",
+                    NormalizedName = "Staff".ToUpper()
+                }
+                );
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Customer",
+                    NormalizedName = "Customer".ToUpper()
+                }
+                );
+        }
+
+
     }
 }
---
+++ EstateAgents/Models/CustomUserModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace EstateAgents.Models
+{
+    public class CustomUserModel : IdentityUser
+    {
+        [Required]
+        [MaxLength(100, ErrorMessage ="Name cannot exceed 100 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(11, ErrorMessage = "Telephone numbers cannot exceed 11 characters.")]
+        public string Telephone { get; set; }
+        [Required]
+        public double Income { get; set; }
+        [Required]
+        [MaxLength(300, ErrorMessage = "Address cannot exceed 300 characters.")]
+        public string Address { get; set; }
+    }
+}
