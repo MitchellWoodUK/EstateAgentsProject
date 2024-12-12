fa4daeaab20331abdced0fe8a88d11d05318e036
Mitchell Wood <WOODM@bridgwater.ac.uk>
2024-12-12 11:45:26 +0000

    Removed the roles from applicationdbcontext

--- EstateAgents/Data/ApplicationDbContext.cs
+++ EstateAgents/Data/ApplicationDbContext.cs
@@ -13,40 +13,5 @@ namespace EstateAgents.Data
         }
 
 
-        //Seed Roles Below
-        /*private override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }*/
-
-        private void SeedRoles(ModelBuilder builder)
-        {
-            builder.Entity<IdentityRole>().HasData(
-                new IdentityRole()
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Name = "Admin",
-                    NormalizedName = "Admin".ToUpper()
-                }
-                );
-            builder.Entity<IdentityRole>().HasData(
-                new IdentityRole()
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Name = "Staff",
-                    NormalizedName = "Staff".ToUpper()
-                }
-                );
-            builder.Entity<IdentityRole>().HasData(
-                new IdentityRole()
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Name = "Customer",
-                    NormalizedName = "Customer".ToUpper()
-                }
-                );
-        }
-
-
     }
 }
