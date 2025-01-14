b89e1cbb411078b6dfa91b0152f3d8d3beef40dd
Mitchell Wood <WOODM@bridgwater.ac.uk>
2025-01-14 16:20:12 +0000

    Can now add properties and view them

--- EstateAgents/Controllers/PropertyController.cs
+++ EstateAgents/Controllers/PropertyController.cs
@@ -1,4 +1,5 @@
 ﻿using EstateAgents.Data;
+using EstateAgents.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EstateAgents.Controllers
@@ -16,7 +17,10 @@ namespace EstateAgents.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            //Retrieve the properties from the database
+            var properties = _context.Properties.ToList();
+
+            return View(properties); //Send the list to the view
         }
 
         //Get: Property/Create
@@ -27,6 +31,55 @@ namespace EstateAgents.Controllers
         }
 
 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(PropertyModel property, IFormFile imageFile)
+        {
+            //Check if the image has been sent to the backend
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                //Define the path to save the image in
+                var imagesDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images");
+
+                if (!Directory.Exists(imagesDirectory))
+                {
+                    //If it does not already exist, create it!
+                    Directory.CreateDirectory(imagesDirectory);
+                }
+                //Creating the filepath to save in the database
+                var filePath = Path.Combine(imagesDirectory, imageFile.FileName);
+
+
+                try
+                {
+                    //save the uploaded image to the server
+                    using(var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        imageFile.CopyTo(stream);
+                    }
+
+                    //Set the image file name into the PropertyModel
+                    property.Image = imageFile.FileName;
+
+                    //Add the listing date
+                    property.ListingDate = DateTime.Now;
+
+                    _context.Properties.Add(property);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch(Exception ex)
+                {
+                    ModelState.AddModelError("Image", "An error occured while saving the image. Please try again!");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("Image", "An error occured while saving the image. Please try again!");
+            }
+            return View(property);
+        }
 
     }
 }
